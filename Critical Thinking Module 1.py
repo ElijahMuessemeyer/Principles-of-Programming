bf619c90950f8d4245309227cac0534bbb836732
ElijahMuessemeyer <david.muessemeyer@csuglobal.edu>
2024-07-13 15:18:18 -0500

    Add files via upload

---
+++ Critical Thinking Module 1.py
@@ -0,0 +1,9 @@
+user_numbers = input("Input two numbers, separated by a space: ")
+numbers = user_numbers.split()
+num1 = int(numbers[0])
+num2 = int(numbers[1])
+
+print("The sum of these numbers is ", num1 + num2)
+print("The difference of these numbers is ", num1 - num2)
+print("The product of these numbers is ", num1 * num2)
+print("The quotient of these numbers is ", num1 / num2)
